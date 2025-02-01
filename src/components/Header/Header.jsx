b6895c58274cb30cbd10ee1e549ecd6439baa739
Alymbekdd <usekowalymbek2002@gmail.com>
2025-02-01 13:43:35 +0600

    1
    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import bg from '../img/IMG_1498.JPG';
+import bg from '../img/IMG_1558.JPG';
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
@@ -8,21 +8,22 @@ const Header = () => {
     const isBuyPage = location.pathname.startsWith("/Buy");
     const isCartPage = location.pathname.startsWith("/Cart");
 
+    
     return (
         <header className={`Header ${isBuyPage || isCartPage ? "Header__shadow" : ""}`}>
             {isMain && <img src={bg} alt="Фон" className="Header__bg" />}
             <div class="container">
                 <div className="Header__content">
-                    <h2 className={`Header__content-logo ${isBuyPage || isCartPage ? "Header__content-black" : ""}`}>Phones</h2>
+                    <h2 className='Header__content-logo'>Phones</h2>
                     <nav>
                         <ul>
-                            <Link className={`Header__content-link ${isBuyPage || isCartPage ? "Header__content-black" : ""}`} to={'/'}>
+                            <Link className='Header__content-link' to={'/'}>
                                 <li>Главная</li>
                             </Link>
-                            <Link className={`Header__content-link ${isBuyPage || isCartPage ? "Header__content-black" : ""}`}>
+                            <Link className='Header__content-link'>
                                 <li>Контакты</li>
                             </Link>
-                            <Link className={`Header__content-link ${isBuyPage || isCartPage ? "Header__content-black" : ""}`} to={'/Cart'}>
+                            <Link className='Header__content-link' to={'/Cart'}>
                                 <li>Корзина</li>
                             </Link>
                         </ul>
